a40241ab962ab8614bf23d7cbf8fd4e600495fc8
Grace Kim <goooyoon@gmail.com>
2020-05-29 21:57:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,14 @@ def roll_call_dwarves(dwarves)
  puts "#{index[0]}, #{dwarves}"
 end
 
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+
+
+
 def summon_captain_planet# code an argument here
   # Your code here
 end
